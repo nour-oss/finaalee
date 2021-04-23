4d077aa7b29f76e1b6c97ba151599b4fc8dfb782
nour-oss <nour.bouali@esprit.tn>
2021-04-23 11:28:17 +0100

    mm

---
+++ migrations/Version20210422161307.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210422161307 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE examen_question (examen_id INT NOT NULL, question_id INT NOT NULL, INDEX IDX_8A572DF85C8659A (examen_id), INDEX IDX_8A572DF81E27F6BF (question_id), PRIMARY KEY(examen_id, question_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE examen_question ADD CONSTRAINT FK_8A572DF85C8659A FOREIGN KEY (examen_id) REFERENCES examen (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE examen_question ADD CONSTRAINT FK_8A572DF81E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE cours ADD favoris TINYINT(1) DEFAULT NULL, CHANGE titre titre VARCHAR(1024) DEFAULT NULL, CHANGE fichier fichier VARCHAR(1024) DEFAULT NULL, CHANGE formation_id formation_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE examenquestion ADD CONSTRAINT FK_613FCF17325D2776 FOREIGN KEY (idExamen) REFERENCES examen (id)');
+        $this->addSql('ALTER TABLE examenquestion ADD CONSTRAINT FK_613FCF17E5546315 FOREIGN KEY (idQuestion) REFERENCES question (id)');
+        $this->addSql('ALTER TABLE listereponse ADD CONSTRAINT FK_97F44C7F4F0FB535 FOREIGN KEY (idReponse) REFERENCES reponse (id)');
+        $this->addSql('ALTER TABLE listereponse ADD CONSTRAINT FK_97F44C7FE5546315 FOREIGN KEY (idQuestion) REFERENCES question (id)');
+        $this->addSql('ALTER TABLE note ADD CONSTRAINT FK_CFBDFA14325D2776 FOREIGN KEY (idExamen) REFERENCES examen (id)');
+        $this->addSql('ALTER TABLE reponse CHANGE idQuestion idQuestion INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE reponse ADD CONSTRAINT FK_5FB6DEC7E5546315 FOREIGN KEY (idQuestion) REFERENCES question (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE examen_question');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE cours DROP favoris, CHANGE titre titre VARCHAR(1024) CHARACTER SET utf8 NOT NULL COLLATE `utf8_general_ci`, CHANGE fichier fichier VARCHAR(1024) CHARACTER SET utf8 NOT NULL COLLATE `utf8_general_ci`, CHANGE formation_id formation_id INT NOT NULL');
+        $this->addSql('ALTER TABLE examenquestion DROP FOREIGN KEY FK_613FCF17325D2776');
+        $this->addSql('ALTER TABLE examenquestion DROP FOREIGN KEY FK_613FCF17E5546315');
+        $this->addSql('ALTER TABLE listereponse DROP FOREIGN KEY FK_97F44C7F4F0FB535');
+        $this->addSql('ALTER TABLE listereponse DROP FOREIGN KEY FK_97F44C7FE5546315');
+        $this->addSql('ALTER TABLE note DROP FOREIGN KEY FK_CFBDFA14325D2776');
+        $this->addSql('ALTER TABLE reponse DROP FOREIGN KEY FK_5FB6DEC7E5546315');
+        $this->addSql('ALTER TABLE reponse CHANGE idQuestion idQuestion INT NOT NULL');
+    }
+}
---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Examen;
+use App\Entity\Formation;
+use App\Entity\Question;
+use App\Entity\Reponse;
+use App\Form\QuestionType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/question")
+ */
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/", name="question_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $questions = $this->getDoctrine()
+            ->getRepository(Question::class)
+            ->findAll();
+            $formations = $this->getDoctrine()
+            ->getRepository(Formation::class)
+            ->findAll();
+
+        return $this->render('question/index.html.twig', [
+            'questions' => $questions,
+            'formations' => $formations
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="question_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $question = new Question();
+        $form = $this->createForm(QuestionType::class, $question);
+        $form->handleRequest($request);
+        $examens = $this->getDoctrine()->getRepository(Examen::class)->findAll();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+           
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($question);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('question_new');
+        }
+
+        return $this->render('question/new.html.twig', [
+            'question' => $question,
+            'form' => $form->createView(),
+            'examens' => $examens
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="question_show", methods={"GET"})
+     */
+    public function show(Question $question): Response
+    {
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="question_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Question $question): Response
+    {
+        $form = $this->createForm(QuestionType::class, $question);
+        $form->handleRequest($request);
+        $examens = $this->getDoctrine()->getRepository(Examen::class)->findAll();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('question_index');
+        }
+
+        return $this->render('question/edit.html.twig', [
+            'question' => $question,
+            'form' => $form->createView(),
+            'examens' => $examens
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="question_delete")
+     */
+    public function delete(Request $request, Question $question): Response
+    {
+        
+            $entityManager = $this->getDoctrine()->getManager();
+            $rep = $this->getDoctrine()->getRepository(Reponse::class)->findOneBy(['idquestion' =>$question->getId()]);
+            $entityManager->remove($question);
+            if ($rep != NULL)
+            $entityManager->remove($rep);
+            $entityManager->flush();
+       
+
+        return $this->redirectToRoute('question_index');
+    }
+}
